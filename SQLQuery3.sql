7ce417d3e962c51a1e9de5fc508972ca0f76ba2d
Shane Hurtado <mhurtado1@berklee.edu>
2023-03-21 15:51:34 -0500

    searches for posts, and blogs

---
+++ SQLQuery3.sql
@@ -0,0 +1,6 @@
+﻿SELECT b.id,
+                                            b.Title,
+                                               b.Url, t.Id
+                                          FROM Blog b
+                                               LEFT JOIN BlogTag bt on b.Id = bt.BlogId
+                                               LEFT JOIN Tag t on t.Id = bt.TagId
